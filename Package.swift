fd35d8f71313bb8bf423f56f5f1b5ef2a234c2c4
Greg Cotten <gregcotten@users.noreply.github.com>
2022-12-14 22:07:14 -0600

    silence some warnings

--- Package.swift
+++ Package.swift
@@ -14,13 +14,16 @@ let package = Package(
         .target(
             name: "CZlib",
             publicHeadersPath: "include",
-            cSettings: [.define("Z_HAVE_UNISTD_H", .when(platforms: [.macOS, .linux])),
-                        .define("HAVE_STDARG_H"),
-                        .define("HAVE_HIDDEN")]),
+            cSettings: [
+                .unsafeFlags(["-Wno-shorten-64-to-32"]),
+                .define("Z_HAVE_UNISTD_H", .when(platforms: [.macOS, .linux])),
+                .define("HAVE_STDARG_H"),
+                .define("HAVE_HIDDEN"),
+                .define("_CRT_SECURE_NO_DEPRECATE", .when(platforms: [.windows])),
+                .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]),
         .testTarget(
             name: "CZlibTests",
             dependencies: ["CZlib"]),
     ]
 )
-
-
